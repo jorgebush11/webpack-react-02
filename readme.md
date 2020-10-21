4ca229b8c97f4fe9272c63e91b218c7e0072cbc7
arthursmith <arthursmith@mail.com>
2020-10-21 08:35:39 -0500

    Initial structure and dependencies

---
+++ readme.md
@@ -0,0 +1,6 @@
+# React setup with webpack and babel
+
+React with webpack 5 and babel
+
+**Tutorial followed:**
+[Setting up react with webpack](https://www.youtube.com/watch?v=e_oGhRHDzcc&t=264s)
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+	return (
+		<div>
+			<h1>React setup with Webpck 5 and Babel</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+				repudiandae!
+			</p>
+		</div>
+	)
+}
+
+export default App
